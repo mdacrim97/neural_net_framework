36630ffd6762789a3e18012d0b5f6fdf7a41df12
mdacrim97 <mdacrim97@gmail.com>
2020-08-24 21:15:07 -0400

    added activation functions and their derivatives. 1 constructor nearly done for activationFunction class.

--- activation_function.cpp
+++ activation_function.cpp
@@ -1,38 +1,128 @@
 #include "activation_function.hpp"
 
-ActivationFunction::ActivationFunction((*pActivation)(double), (*pActivationDerivative)(double));
-ActivationFunction::ActivationFunction(string function)
-
-double ActivationFunction::sigmoid(double input){
+ActivationFunction::ActivationFunction((*pActivation)(double), (*pActivationDerivative)(double)){
 
 }
-double ActivationFunction::sidmoidDerivative(double input){
+ActivationFunction::ActivationFunction(string function){
+
+	if(function = "sigmoid"){
+		pFunction = sigmoid;
+		pDerivative = sigmoidDerivative;
+	}
+	else if(function = "linear"){
+		pFunction = linear;
+		pDerivative = linearDerivative;
+	}
+	else if(function = "elu"){
+		pFunction = elu;
+		pDerivative = eluDerivative;
+	}
+	else if(function = "reLu"){
+		pFunction = reLu;
+		pDerivative = reLuDerivative;		
+	}
+	else if(function = "leaky_reLu"){
+		pFunction = leakyRelu;
+		pDerivative = leakyReluDerivative;	
+	}
+	else if(function = "tanh"){
+		pFunction = tanh;
+		pDerivative = tanhDerivative;
+	}
+	else if(function = "swish"){
+		pFunction = swish;
+		pDerivative = swishDerivative;	
+	}
+	else{
+		  
+	}
+
+	this->name = function;
 
 }
 
-double ActivationFunction::tanh(double input){
+double ActivationFunction::sigmoid(double z){
+	return 1/(1 + math.pow(M_E, -z))
 
 }
-double ActivationFunction::tanhDerivative(double input){
-
+double ActivationFunction::sigmoidDerivative(double z){
+	return sigmoid(z) * 1 (1- sigmoid(z))
 }
 
-double ActivationFunction::reLu(double input){
-
-}
-double ActivationFunction::reLuDerivative(double input){
+double ActivationFunction::linear(double z, double alpha=1){
+	return alpha * z;
 }
 
-double ActivationFunction::leakyReLu(double input){
-
-}
-double ActivationFunction::leakyReLuDerivative(double input){
-
+double ActivationFunction::linearDerivative(double z, double alpha=1){
+	return alpha;
 }
 
-double swish(double input){
-
+double ActivationFunction::elu(double z, double alpha=1){
+	
+	if(z>0)
+		return z;
+	else
+		return alpha *(math.pow(M_E, z) - 1);
 }
-double swishDerivative(double input){
 
+double ActivationFunction::eluDerivative(){
+	
+	if(z >0)
+		return 1;
+	else 
+		return alpha * math.pow(M_E, z);
+}
+
+double ActivationFunction::tanh(double z){
+	
+	double posPow = math.pow(M_E, z),
+		   negPow = math.pow(M_E, -z);
+
+	return (posPow - negPow)/(posPow + negPow;/)
+}
+
+double ActivationFunction::tanhDerivative(double z){
+
+	return 1 - math.pow(tanh(z), 2)
+}
+
+double ActivationFunction::reLu(double z){
+
+	if( z > 0)
+		return z;
+	else
+		return 0;
+}
+
+double ActivationFunction::reLuDerivative(double z){
+
+	if (z > 0)
+		return 1;
+	else
+		return 0;
+}
+
+double ActivationFunction::leakyReLu(double z, double alpha=1){
+
+	if( z > 0)
+		return z;
+	else
+		return alpha*z;
+}
+
+double ActivationFunction::leakyReLuDerivative(double z, double alpha=1){
+
+	if( z > 0)
+		return 1;
+	else
+		return alpha;
+}
+
+double swish(double z){
+	return z * sigmoid(z);
+}
+
+double swishDerivative(double z){
+	result = swish(z)
+	return result + sigmoid(z)*(1-result)
 }
--- activation_function.hpp
+++ activation_function.hpp
@@ -1,5 +1,5 @@
 #ifndef ACTIVATION_FUNCTION
-#define ACTIVATION_FUNCTINO
+#define ACTIVATION_FUNCTION
 
 #define _USE_MATH_DEFINES
 
@@ -15,23 +15,31 @@ class ActivationFunction{
 	private:
 		double (*pFunction)(double),
 			   (*pDerivative)(double);	
+		
+		string name;
 
 
 		//provided activation functions
-		double sigmoid(double input);
-		double sidmoidDerivative(double input);
+		double sigmoid(double z);
+		double sigmoidDerivative(double z);
+
+		double linear(double z);
+		double linearDerivative(double z);
+
+		double ActivationFunction::elu(double z, double alpha=1){
+		double ActivationFunction::eluDerivative(double z, double alpha=1){
 
 		double tanh(double input);
-		double tanhDerivative(double input);
+		double tanhDerivative(double z);
 
 		double reLu(double input);
-		double reLuDerivative(double input);
+		double reLuDerivative(double z);
 
-		double leakyReLu(double input);
-		double leakyReLuDerivative(double input);
+		double leakyReLu(double input, double alpha=1);
+		double leakyReLuDerivative(double z, double alpha=1);
 
 		double swish(double input);
-		double swishDerivative(double input);
+		double swishDerivative(double z);
 		
 
 	public:
